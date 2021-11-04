8bb0de00bf4245fec5c716314dbb42da40206d22
Seyed Sepehr Sahraian <68083280+sprshr@users.noreply.github.com>
2021-11-04 17:31:32 +0000

    Columns added

--- connect4.py
+++ connect4.py
@@ -1,17 +1,51 @@
-row1 = ["|     |","|     |","|     |","|     |","|     |","|     |","|     |"]
-row2 = ["|     |","|     |","|     |","|     |","|     |","|     |","|     |"]
-row3 = ["|     |","|     |","|     |","|     |","|     |","|     |","|     |"]
-row4 = ["|     |","|     |","|     |","|     |","|     |","|     |","|     |"]
-row5 = ["|     |","|     |","|     |","|     |","|     |","|     |","|     |"]
-row6 = ["|     |","|     |","|     |","|     |","|     |","|     |","|     |"]
-print(row1)
-print()
-print(row2)
-print()
-print(row3)
-print()
-print(row4)
-print()
-print(row5)
-print()
-print(row6)
+print("connect4 By Sepehr Sahraian")
+print("AP CSP Mr. Keithley")
+columnNumber = [
+    "   1   ", "   2   ", "   3   ", "   4   ", "   5   ", "    6   ", "   7   "
+]
+row1 = [
+    "|     |", "|     |", "|     |", "|     |", "|     |", "|     |", "|     |"
+]
+row2 = [
+    "|     |", "|     |", "|     |", "|     |", "|     |", "|     |", "|     |"
+]
+row3 = [
+    "|     |", "|     |", "|     |", "|     |", "|     |", "|     |", "|     |"
+]
+row4 = [
+    "|     |", "|     |", "|     |", "|     |", "|     |", "|     |", "|     |"
+]
+row5 = [
+    "|     |", "|     |", "|     |", "|     |", "|     |", "|     |", "|     |"
+]
+row6 = [
+    "|     |", "|     |", "|     |", "|     |", "|     |", "|     |", "|     |"
+]
+
+column1 = []
+column2 = []
+column3 = []
+column4 = []
+column5 = []
+column6 = []
+column7 = []
+
+
+def execute():
+	print("I'm X You're O, Lets Go!")
+	print('')
+	print(columnNumber)
+	print('')
+	print(row1)
+	print('')
+	print(row2)
+	print('')
+	print(row3)
+	print('')
+	print(row4)
+	print('')
+	print(row5)
+	print('')
+	print(row6)
+
+execute()
