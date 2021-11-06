92b4ee697771c3e90ba54ac0970bfcdb3a35ce31
Sepehr Sahraian <sep3831@gmail.com>
2021-11-05 23:59:33 -0700

    selection input created

--- connect4.py
+++ connect4.py
@@ -1,7 +1,8 @@
 print("connect4 By Sepehr Sahraian")
 print("AP CSP Mr. Keithley")
+print('')
 columnNumber = [
-    "   1   ", "   2   ", "   3   ", "   4   ", "   5   ", "    6   ", "   7   "
+    "   1   ", "   2   ", "   3   ", "   4   ", "   5   ", "   6   ", "   7   "
 ]
 row1 = [
     "|     |", "|     |", "|     |", "|     |", "|     |", "|     |", "|     |"
@@ -30,22 +31,34 @@ column5 = []
 column6 = []
 column7 = []
 
+#global var
+xSelected = "|  X  |"
+oSelected = "|  O  |"
 
-def execute():
-	print("I'm X You're O, Lets Go!")
-	print('')
-	print(columnNumber)
-	print('')
-	print(row1)
-	print('')
-	print(row2)
-	print('')
-	print(row3)
-	print('')
-	print(row4)
-	print('')
-	print(row5)
-	print('')
-	print(row6)
 
-execute()
+def printTable():
+    print('')
+    print(columnNumber)
+    print('')
+    print(row1)
+    print('')
+    print(row2)
+    print('')
+    print(row3)
+    print('')
+    print(row4)
+    print('')
+    print(row5)
+    print('')
+    print(row6)
+
+
+def selection():
+    print("Make a Selection 1-7")
+    global columnSelected
+    columnSelected = input()
+
+
+print("I'm X you're O Let's go!")
+printTable()
+selection()
